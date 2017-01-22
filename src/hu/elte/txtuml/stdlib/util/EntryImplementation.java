ed6a2b30037e48337b2f9695465671c43d2a1324
TatianaCarrascoOrtiz <yeca500@gmail.com>
2017-01-22 20:46:09 +0100

    Add files via upload
    Fixing all your requirements
    Thank you

---
+++ src/hu/elte/txtuml/stdlib/util/EntryImplementation.java
@@ -0,0 +1,37 @@
+package hu.elte.txtuml.stdlib.util;
+
+public class EntryImplementation implements Entry {
+	String key;
+	String value;
+
+	public EntryImplementation(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public boolean equals(Entry entry) {
+		if (entry instanceof EntryImplementation) {
+			EntryImplementation entry_class = (EntryImplementation) entry;
+			return key.equals(entry_class.key) && value.equals(entry_class.value);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String getKey() {
+		return key;
+	}
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public String setValue(String v) {
+		String old_value = value;
+		value = v;
+		return old_value;
+	}
+}
