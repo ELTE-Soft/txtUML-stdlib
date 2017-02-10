c643c7891991ecc7db2118af7c36087e8ccd0d5f
rakoti <mokwadr@gmail.com>
2017-02-10 15:44:51 +0100

    Update PatternImplementation.java

--- src/hu/elte/txtuml/stdlib/util/regex/PatternImplementation.java
+++ src/hu/elte/txtuml/stdlib/util/regex/PatternImplementation.java
@@ -1,48 +1,35 @@
-package RegularExpression;
+package hu.elte.txtuml.stdlib.util.regex;
 
 import hu.elte.txtuml.api.model.Collection;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
-/**	
-*An implementation of pattern Interface that represents Pattern.
-*@see regular expression(regex) java.util.Regex 
-*/
-public class PatternImplementation implements Pattern{
+public class PatternImplementation implements hu.elte.txtuml.stdlib.util.regex.Pattern{
 	
-	java.util.regex.Pattern pattern;
-	java.util.regex.Matcher matcher;
+	Pattern pattern;
+	Matcher matcher;
 	
 	public PatternImplementation(){
 	}
 	
-	public PatternImplementation(java.util.regex.Pattern pattern){
+	public PatternImplementation(Pattern pattern){
 		this.pattern = pattern;
 	}
 
-	public PatternImplementation(java.util.regex.Matcher matcher){
+	public PatternImplementation(Matcher matcher){
 		this.matcher = matcher;
 	}
 	
-	/**
-	 *Returns this pattern's match flags.
-	 *@see java.util.regex.Pattern.flags(). 
-	 */
 	@Override
 	public int flags() {
 		return pattern.flags();
 	}
-	/**
-	 * Returns the regular expression from which this pattern was compiled.
-	 * @see java.util.regex.Pattern.pattern()
-	 */
+	
 	@Override
 	public String pattern() {
 		return pattern.pattern();
 	}
 	
-	/**
-	 *Returns a literal pattern String for the specified input.
-	 *@see java.util.regex.Pattern.split(String input). 
-	 */
 	@Override
 	public Collection<String> split(String input) {
 		String[] arr = pattern.split(input);
@@ -53,10 +40,7 @@ public class PatternImplementation implements Pattern{
 		
 		return result;
 	}
-	/**
-	 * Splits the given input sequence around matches of this pattern. 
-	 * @see java.util.regex.Pattern.split(String input, int limit).
-	 */
+	
 	@Override
 	public Collection<String> split(String input, int limit) {
 		String[] arr = pattern.split(input, limit);
@@ -67,32 +51,14 @@ public class PatternImplementation implements Pattern{
 		return result;
 	}
 	
-	/**
-	 * Returns the string representation of this pattern.
-	 * @see java.util.regex.Pattern.toString().
-	 */
 	@Override
 	public String toString() {
 		return pattern.toString();
 	}
 	
-	/**
-	 * Creates a matcher that will match the given input against this pattern.
-	 * @see java.util.regex.matcher(String input) 
-	 */
 	@Override
 	public MatcherImplementation matcher(String input){
 		MatcherImplementation myMatcher = new MatcherImplementation(pattern.matcher(input));
 		return myMatcher;
 	}
 }
-
-/*Unimpplemented functions
-@Override
-public Predicate<String> asPredicate(){
-return pattern.asPredicate();
-
-@Override
-public Stream<String> splitAsStream(CharSequence input){
-return pattern.splitAsStream(input);
-}*/
